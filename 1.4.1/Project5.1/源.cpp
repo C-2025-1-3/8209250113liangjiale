b0a839d9fbe5e4dd7e2d915554afdc9c56a956cf
Liang-219 <1597821568@qq.com>
2025-11-17 15:22:31 +0800

    Add files via upload

---
+++ 1.4.1/Project5.1/源.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+	 float l;
+	 cin >> l;
+	unsigned int testUnint = 65534;//oxfffe
+	cout << "output in unsigned int 1 type:""" << testUnint<< endl;//<<oct;
+		cout << "output in char type:!" << static_cast<char>(testUnint) << endl;
+	cout << "output in short type:" << static_cast<short>(testUnint) << endl;//为什么结果为-2?:
+	cout << "output in int type:" << static_cast<int>(testUnint) << endl;
+	cout << "output in double type:" << static_cast<double>(testUnint) << endl;
+	cout << "output in double type:" << setprecision(4) << static_cast<double>(testUnint) << endl;
+	cout << "output in Oct unsigned int type:" << oct << testUnint << endl; //8进制输出
+	cout << "output in int type:" << static_cast<int>(l) << endl;//将一个实数转换成int
+	system("pause");
+	return 0;
+}
