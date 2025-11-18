1da652a0ebf8ddf5d0eeaccc3602b4fd02ec0483
Liang-219 <1597821568@qq.com>
2025-11-18 22:28:47 +0800

    Add files via upload

---
+++ 1.5.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main() {
+	float c,f;
+	cout << "华氏温度是：";
+	cin >> f;
+	c = (f - 32) * 5 / 9;
+	cout << "摄氏温度是：" << fixed<<setprecision(2)<<c<<endl;
+
+	return 0;
+}
